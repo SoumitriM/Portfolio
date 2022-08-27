d11bb134b7548511d867d694705ed8915df04fc9
Soumitri Mukherjee <soumitri@Soumitris-MacBook-Air.local>
2022-08-27 15:28:00 +0530

    added a new project in projects

--- src/projectList.js
+++ src/projectList.js
@@ -12,7 +12,7 @@ const projectList = [
 		altText: "A twitter clone",
 		name: "Twitter UI",
 		description: "Using React JS, Firebase API, Firebase Realtime Database",
-		link: "https://chitchat-f9412.firebaseapp.com/"
+		link: "https://react-smuk.web.app/"
 	},
 	{
 		projectPicture: 'https://firebasestorage.googleapis.com/v0/b/react-smuk.appspot.com/o/coverpictures%2FsouId98?alt=media&token=64ebdc8b-d189-4928-904e-fffd6d96a127',
@@ -23,8 +23,9 @@ const projectList = [
 	{
 		projectPicture: 'https://firebasestorage.googleapis.com/v0/b/react-smuk.appspot.com/o/coverpictures%2FsouId98?alt=media&token=64ebdc8b-d189-4928-904e-fffd6d96a127',
 		altText: "Full stack Social Media",
-		name: "Full Stack New",
-		description: "Using React JS for frontend, Node JS API and MongoDb database"
+		name: "Portfolio",
+		description: "Using React JS for frontend and deployed in Github pages",
+		link: "https://soumitrim.github.io/Portfolio/"
 	}
 ];
 
